5c938f315b274610d8305e0d3e07c240c37a580f
Dharshib <Dharshib.8@gmail.com>
2023-09-11 09:17:46 +0530

    add Home component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import './App.css';
+import NavBar from './components/navbar/Navbar';
+import Home from './components/Home/Home';
+// import About from './components/About/About';
 
 function App() {
   return (
     <>
+      <NavBar/>
+      <Home/>
+      {/* <About/> */}
     </>
   );
 }
---
+++ src/components/Home/Home.js
@@ -0,0 +1,15 @@
+import './Home.css';
+
+function Home() {
+    return (
+        <section id='home'>
+           <div>
+                <p>The art of welcoming you since 2000!</p>
+                <p>The most praised and gourmet restaurent in the heart of Sri Lanka</p>
+                <button>Order Now</button>
+            </div>
+        </section>
+    );
+}
+
+export default Home;
