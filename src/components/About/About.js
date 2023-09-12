5cb4d7315973380e0c5e9cbe8ecd1d3932d06f9b
Dharshib <Dharshib.8@gmail.com>
2023-09-12 14:51:01 +0530

    add about componant

---
+++ src/components/About/About.js
@@ -0,0 +1,23 @@
+import './About.css';
+
+function About() {
+
+    return (
+        <section id='about'>
+            <h1>About Us</h1>
+            <div>
+                <div className='image'></div>
+                <div className='para'>
+                    <h2>Enjoy the Real Taste!</h2><br></br>
+                    <p>We create a warm and welcoming atmosphere where every guest feels like family, and where the quality of our food and service is unmatched. Our goal is to be a restaurant that not only nourishes the body, but also the soul.</p>
+                    <br></br><p>At Berries, our mission is to using only the freshest and highest-quality ingredients, sourced locally whenever possible, and to preparing each dish with care and attention to detail. time.</p>
+                    <br></br><p>Whether you're looking for a casual lunch spot, a romantic dinner for two, Olive Branch has got you covered. So come on in, grab a seat, and let us take care of the rest.</p>
+                    <button>Learn More</button>
+                </div>
+            </div>
+        </section>
+    );
+
+}
+
+export default About;
