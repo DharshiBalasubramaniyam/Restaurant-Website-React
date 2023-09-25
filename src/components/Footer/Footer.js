9aa13c8927072b5aa554de367d7f6a676f49c3b7
Dharshib <Dharshib.8@gmail.com>
2023-09-25 12:17:09 +0530

    update footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -10,7 +10,7 @@ function Footer() {
     return(
         <>
             <footer>
-                <div class="footer-box">
+                <div className="footer-box">
                     <h1>Follow Us</h1>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. <br></br>Libero, quidem.</p>
                     <div>
@@ -19,21 +19,21 @@ function Footer() {
                         <a href="#"><AiFillYoutube size='25px' style={{marginRight:'10px'}}/></a>
                     </div>
                 </div>
-                <div class="footer-box">
+                <div className="footer-box">
                     <h1>Need Help?</h1>
                     <p>Lorem ipsum dolor sit.</p>
                     <a href="#"><AiFillPhone style={{marginRight:'7px'}}/>+87 123456789</a>
                     <a href="#"><AiFillPhone style={{marginRight:'7px'}}/>+87 987654321</a>
                     <a href="#"><MdEmail style={{marginRight:'7px'}}/>olivebranch@gmail.com</a>
                 </div>
-                <div class="footer-box">
+                <div className="footer-box">
                     <h1>Locations</h1>
                     <a href="#">Colombo</a>
                     <a href="#">Galle</a>
                     <a href="#">Hatton</a>
                     <a href="#">Jaffna</a>
                 </div>
-                <div class="footer-box">
+                <div className="footer-box">
                     <h1>Quick Links</h1>
                     <a href="#">Sign In</a>
                     <a href="#">Reviews</a>
